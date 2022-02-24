89531d81035a2a1d54516532d0867a35dacd5133
sammysamp <sammysamp@gmail.com>
2022-02-24 13:38:19 -0800

    changing user limits using pp manifest

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# Editing security limits to specific user
+exec {'Updating limits to Holberton User':
+  command  => "sed -i 's/holberton soft nofile.*/holberton soft nofile 9000/' /etc/security/limits.conf",
+  provider => shell
+}
+exec {'Updating limit to Holberton User':
+  command  => "sed -i 's/holberton hard nofile.*/holberton hard nofile 9000/' /etc/security/limits.conf",
+  provider => shell
+}
